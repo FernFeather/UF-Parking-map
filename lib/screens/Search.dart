7cfc55f2b06759a12d87a4af60db163e40c4aae7
Juliana Lucas <63823731+FernFeather@users.noreply.github.com>
2022-12-01 20:26:06 -0500

    update to allow for user location

--- lib/screens/Search.dart
+++ lib/screens/Search.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:provider/provider.dart';
 
 class Search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final currentPosition = Provider.of<Position?>(context);
+
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        Container(
-            height: MediaQuery.of(context).size.height / 3,
-            width: MediaQuery.of(context).size.width,
-            child: GoogleMap(
-                initialCameraPosition: CameraPosition(
-                    target: LatLng(29.6516, -82.3248), zoom: 8.0)))
-      ],
-    ));
+        body: (currentPosition != null)
+            ? Column(
+                children: <Widget>[
+                  Container(
+                      height: MediaQuery.of(context).size.height / 3,
+                      width: MediaQuery.of(context).size.width,
+                      child: GoogleMap(
+                          zoomGesturesEnabled: true,
+                          initialCameraPosition: CameraPosition(
+                              target: LatLng(currentPosition.latitude,
+                                  currentPosition.longitude),
+                              //target: LatLng(29.6328784, -82.34901329340119),
+                              zoom: 16.0)))
+                ],
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ));
   }
 }
 
