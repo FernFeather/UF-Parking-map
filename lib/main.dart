2430a307d77152dbefa9c36e08c9b5409ccecfd6
Juliana Lucas <63823731+FernFeather@users.noreply.github.com>
2022-12-01 20:27:08 -0500

    allow for user input

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:parkings_/screens/Search.dart';
+import 'package:parkings_/services/geolocateService.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+  final locatorService = geolocateService();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return FutureProvider(
+      initialData: null,
+      create: (context) => locatorService.getLocation(),
+      child: MaterialApp(
+        title: 'Parkbois',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        //home: const MyHomePage(title: 'Parkkkkkkoings'),
+        home: Search(),
       ),
-      //home: const MyHomePage(title: 'Parkkkkkkoings'),
-      home: Search(),
     );
   }
 }
