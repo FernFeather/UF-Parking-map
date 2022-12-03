d0ea1afa4b09ddfb1207c7d72b3b0f49a59b36e7
Erik <13103585+FenFideleo@users.noreply.github.com>
2022-12-03 11:39:11 -0500

    Implemented access to list for slide up panel.

--- lib/screens/Search.dart
+++ lib/screens/Search.dart
@@ -3,6 +3,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+import '../services/parking_data.dart';
 
 class Search extends StatelessWidget {
   @override
@@ -19,9 +20,12 @@ class Search extends StatelessWidget {
                 ============== GOOGLE MAPS PANEL ============
                  */
                 Container(
-                  height: MediaQuery.of(context).size.height / 3,
+                  height: MediaQuery.of(context).size.height,
                   width: MediaQuery.of(context).size.width,
                   child: GoogleMap(
+                    padding: EdgeInsets.only(
+                        bottom: MediaQuery.of(context).size.height * (1/12)
+                    ),
                     zoomGesturesEnabled: true,
                     initialCameraPosition: CameraPosition(
                       target: LatLng(currentPosition.latitude,
@@ -36,34 +40,68 @@ class Search extends StatelessWidget {
           ============= SLIDE UP PANEL ==============
            */
             SlidingUpPanel(
-              maxHeight: MediaQuery.of(context).size.height * (2/3),
-              panel: Center(
-                child: Text("This is the sliding Widget"),
-              ),
-              collapsed: Container(
-                decoration: BoxDecoration(
-                  color: Colors.lightBlue[100],
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(24.0),
-                    topRight: Radius.circular(24.0),
+              backdropEnabled: true,
+              backdropOpacity: 0.0,
+              maxHeight: MediaQuery.of(context).size.height * (9/10),
+              minHeight: MediaQuery.of(context).size.height * (1/12),
+              header: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * (1/12),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlue[200],
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(24.0),
+                        topRight: Radius.circular(24.0),
+                      ),
+                    ),
+                    child: const Center(
+                      child: Text(
+                          "Placeholder for search Bar"
+                      ),
+                    ),
                   ),
-                ),
-                child: Center(
-                  child: Text(
-                      "Placeholder for search Bar"
-                  ),
-                ),
+                ],
               ),
-              borderRadius: BorderRadius.only(
+              panelBuilder: (ScrollController sc) => _scrollingList(sc, context),
+              borderRadius: const BorderRadius.only(
                 topLeft: Radius.circular(24.0),
                 topRight: Radius.circular(24.0),
               ),
             ),
           ],
         )
-        : Center(
+        : const Center(
             child: CircularProgressIndicator(),
           ),
+    );
+  }
+
+  Widget _scrollingList(ScrollController sc, BuildContext context) {
+    return ListView.builder (
+      controller: sc,
+      itemCount: ParkingDatabaseService.parkingList.length,
+      padding: EdgeInsets.only(
+          top: MediaQuery.of(context).size.height * (1/12),
+          bottom: 12.0,
+          left: 12.0,
+          right: 12.0,
+      ),
+      itemBuilder: (BuildContext context, int i) {
+        return GestureDetector(
+          child: Card(
+            child: ListTile(
+              title: Text(ParkingDatabaseService.parkingList[i].name!),
+              trailing: const Icon(
+                Icons.keyboard_arrow_right_rounded,
+                color: Colors.lightBlue,
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
